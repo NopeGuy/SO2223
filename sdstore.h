da81e3a41474ee2f292cda523aa7df3452d850a3
JBB-DO-MAL-V2 <100542887+JBB-DO-MAL-V2@users.noreply.github.com>
2022-05-29 21:15:23 +0000

    Delete sdstore.h

--- sdstore.h
+++
@@ -1,9 +0,0 @@
-#define MAX 4096
-
-//Estrutura de dados utilizada para escrita no fifo responsavel pela comunicaçao entre pai e filho
-typedef struct estruturaDadosFifo
-{
-	char opcao[MAX];
-	char comando[MAX];
-	char fifoResposta[MAX];
-}EscritaFifo;
