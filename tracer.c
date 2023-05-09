f0aeb7c6cf77156ae735e2e5de8b3de204393f23
Bernazad <bernardogarcialima@hotmail.com>
2023-05-09 18:55:12 +0100

    Update tracer.c

--- tracer.c
+++ tracer.c
@@ -34,8 +34,9 @@ void itoa(int n, char s[])
         n = -n;         /* make n positive */
     i = 0;
     do
-    {                          /* generate digits in reverse order */
-        if (i >= MAX_STRING_LENGTH) {
+    { /* generate digits in reverse order */
+        if (i >= MAX_STRING_LENGTH)
+        {
             fprintf(stderr, "Error: string too long\n");
             fflush(stderr);
             exit(1);
@@ -48,13 +49,12 @@ void itoa(int n, char s[])
     reverse(s);
 }
 
-
 // funções primarias
 
 // calcula o tempo de execucao do pedido (fazer isto com os valores diretamente ou atraves da struct?)
 suseconds_t calcExec(pedido pedido)
 {
-    return pedido.final - pedido.inicial;
+    return (pedido.final - pedido.inicial)/1000;
 }
 
 // cria um novo fifo para o cliente (através do pid)
@@ -73,7 +73,7 @@ int criaLigacao(pid_t pid)
 char *extraiComandoString(int argc, char **argv)
 {
     char *cmd = malloc(sizeof(char) * 1024); // Aloca um espaço inicial de 1024 bytes
-    memset(cmd, 0, sizeof(char) * 1024); // Preenche todo o espaço alocado com bytes nulos
+    memset(cmd, 0, sizeof(char) * 1024);     // Preenche todo o espaço alocado com bytes nulos
 
     for (int i = 3; i < argc; i++)
     {
@@ -85,11 +85,12 @@ char *extraiComandoString(int argc, char **argv)
 
 void extraiComandoArray(char **str, int argc, char *argv[])
 {
-    int i= 0,j = 3;
-    while(argv[j] && j <= argc){
-    str[i] = argv[j];
-    i++;
-    j++;
+    int i = 0, j = 3;
+    while (argv[j] && j <= argc)
+    {
+        str[i] = argv[j];
+        i++;
+        j++;
     }
     str[i] = '\0';
 }
@@ -97,25 +98,90 @@ void extraiComandoArray(char **str, int argc, char *argv[])
 // Escreve para o stdout o pid que está a correr
 void escrevePID(pid_t pid)
 {
-    char res[35]="Running PID ";
+    char res[35] = "Running PID ";
     char sPID[20];
     itoa(pid, sPID);
     strcat(res, sPID);
-    strcat(res,"\n");
+    strcat(res, "\n");
     write(1, res, sizeof(res));
 }
-// cria (struct)pedido com a informação
-/* Já não é necessario
-pedido criaPedido(pid_t pid, char *commando, time_t inicial,time_t final)
+
+void execute(char *cmd, char **cmds)
 {
-    pedido temp;
-    temp.pid=pid;
-    temp.commando=commando;
-    temp.inicial=inicial;
-    temp.final=final;
-    return temp;
+    pedido pedido;
+    pid_t pid;
+    int pedido_pai[2], status = 0;
+    if (pipe(pedido_pai) == -1)
+    {
+        perror("pedido_pai");
+        _exit(1);
+    }
+
+    if ((pid = fork()) == 0)
+    {
+        // fechar o apontadores de leitura
+        close(pedido_pai[0]);
+
+        escrevePID(getpid());
+
+        // adiciona o tempo inicial e o pid à struct pedido
+        struct timeval inicial;
+        suseconds_t start;
+        gettimeofday(&inicial, NULL);
+
+        pedido.status = 0; // estado do pedido
+        start = inicial.tv_usec;
+        pedido.inicial = start;
+        pedido.pid = getpid();
+        write(pedido_pai[1], &pedido, sizeof(pedido));
+        // falta fazer write para o servidor
+
+        execvp(cmds[0], cmds);
+
+        printf("Programa com o pid %d não concluído!\n", getpid());
+        fflush(stdout);
+        _exit(1);
+    }
+    else if (pid == -1)
+        perror("fork");
+    else
+    {
+        waitpid(pid, &status, 0);
+        if (WIFEXITED(status))
+        {
+            close(pedido_pai[1]);
+            int bytes_read = read(pedido_pai[0], &pedido, sizeof(pedido));
+            if (bytes_read == 0)
+            {
+                printf("erro a ler do pedido_pai");
+                fflush(stdout);
+                _exit(1);
+            }
+            pedido.status = 1;
+
+            struct timeval final;
+            gettimeofday(&final, NULL);
+
+            suseconds_t finish;
+            finish = final.tv_usec;
+            pedido.final = finish;
+
+            // envia para o stdout o tempo de execução do pedido
+            char resposta[25];
+            suseconds_t tempExec = calcExec(pedido);
+            snprintf(resposta, sizeof(resposta), "Ended in %ld ms\n", tempExec);
+            write(STDOUT_FILENO, resposta, strlen(resposta));
+
+            _exit(0);
+        }
+        else
+        {
+            printf("Erro de espera pelo filho %d\n", pid);
+            fflush(stdout);
+            _exit(1);
+        }
+    }
 }
-*/
 
 int main(int argc, char *argv[])
 {
@@ -133,101 +199,19 @@ int main(int argc, char *argv[])
 
     if (argc > 2 && strcmp("execute", argv[1]) == 0 && strcmp(argv[2], "-u") == 0)
     {
-        // Criação do pedido, já com o argc, status a 0
-        pedido pedido;
-        int status = 0;
-        pid_t pid;
         char *cmd;
+        
         cmd = extraiComandoString(argc, argv); // retira o "execute -u" do argc inicial
-        pedido.commando = cmd;//verificar
 
         char **cmds = malloc(sizeof(char *) * (argc - 3));
-        extraiComandoArray(cmds,argc,argv); //poe dar merda
+        extraiComandoArray(cmds, argc, argv);
 
-        // Criação de um pipe para comunicar o output para o pai
-
-        int pedido_pai[2];
-        if (pipe(pedido_pai) == -1)
-        {
-            perror("pedido_pai");
-            _exit(1);
-        }
-
-        if ((pid = fork()) == 0)
-        {
-            // fechar o apontadores de leitura
-            close(pedido_pai[0]);
-
-            escrevePID(getpid());
-
-            // adiciona o tempo inicial e o pid à struct pedido
-            struct timeval inicial;
-            suseconds_t start;
-            gettimeofday(&inicial, NULL);
-
-            pedido.status = 0; // estado do pedido
-            start = inicial.tv_usec;
-            pedido.inicial = start;
-            pedido.pid = getpid();
-            write(pedido_pai[1],&pedido,sizeof(pedido));
-            //falta fazer write para o servidor
-
-            execvp(cmds[0], cmds);                                           
-            
-            printf("Programa com o pid %d não concluído!\n", getpid());
-            fflush(stdout);
-            _exit(1);
-        }
-        else if (pid == -1)
-            perror("fork");
-        else
-        {
-            waitpid(pid, &status, 0);
-            if (WIFEXITED(status))
-            {
-                close(pedido_pai[1]);
-                int bytes_read = read(pedido_pai[0],&pedido,sizeof(pedido));
-                if(bytes_read == 0)
-                {
-                    printf("erro a ler do pedido_pai");
-                    fflush(stdout);
-                    _exit(1);
-                }
-                pedido.status = 1;
-
-                struct timeval final;
-                gettimeofday(&final, NULL);
-                
-                suseconds_t finish;
-                finish = final.tv_usec;
-                pedido.final = finish;
-
-                // envia para o stdout o tempo de execução do pedido
-                char resposta[25];
-                suseconds_t tempExec = calcExec(pedido);
-                snprintf(resposta, sizeof(resposta), "Ended in %ld ms\n", tempExec);
-                write(STDOUT_FILENO, resposta, strlen(resposta));
-
-                _exit(0);
-            }
-            else
-            {
-                printf("Erro de espera pelo filho %d\n", pid);
-                fflush(stdout);
-                _exit(1);
-            }
-        }
+        execute(cmd, cmds);
     }
 
     // caso do status
     else if (argc == 2 && strcmp(argv[1], "status") == 0)
     {
-
-
-
-
-
-
     }
     // caso de comando inválido
     else
