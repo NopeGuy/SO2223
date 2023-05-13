ac51722c97993bf6ac87e6cf2a2412fb94b1b5cb
Bernazad <bernardogarcialima@hotmail.com>
2023-05-13 19:58:46 +0100

    final #:pray: #:100: #:100:

--- src/tracer.h
+++ src/tracer.h
@@ -12,7 +12,6 @@ void escrevePID(pid_t pid);
 char* extraiComandoString(int argc,char** argv);
 void extraiComandoArray(char** str, int argc, char* argv[]);
 pedido criaPedido(pid_t pid, char *commando, time_t inicial,time_t final);
-suseconds_t calcExec(pedido pedido);
 void execute(int write_fd, char *cmd, char **cmds);
 
 
